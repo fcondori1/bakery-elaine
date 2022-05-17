4480b76e492f7f19714f8f1292ae566bfc936b0a
Frankz Condori <frankcondori97@gmail.com>
2022-05-17 12:05:22 -0400

    connect main to app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Main from './components/Main/Main';
 
 function App() {
   return (
     <div>
       Hello World
+      <Main />
     </div>
   );
 }
