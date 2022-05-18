c16e22b53a904fe89b8527ffbbe90a63f4ba3b6c
Frankz Condori <frankcondori97@gmail.com>
2022-05-18 10:27:23 -0400

    add components to app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import Main from './components/Main/Main';
+import Navigation from './components/Navigation/Navigation';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <div>
       Hello World
+      <Navigation />
       <Main />
+      <Footer />
     </div>
   );
 }
