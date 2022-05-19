12794e41e0c7fadd29eb971b142a48493b7dc8a0
Frankz Condori <frankcondori97@gmail.com>
2022-05-19 18:07:49 -0400

    add divs for color boxes

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import './Navigation.css';
 
 function Navigation(props) {
     return (
-        <div>
-            Hello from Navigation
-        </div>
-    );
+			<div>
+				Hello from Navigation
+				<div className='box'>
+					<div className='box1'></div>
+					<div className='box2'></div>
+					<div className='box3'></div>
+					<div className='box4'></div>
+					<div className='box5'></div>
+				</div>
+			</div>
+		);
 }
 
 export default Navigation;
