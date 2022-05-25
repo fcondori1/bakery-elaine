1a7a4db91278986e95fd2062ea800ff5312bdede
Frankz Condori <frankcondori97@gmail.com>
2022-05-25 10:46:37 -0400

    add Links to Navigation

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -3,18 +3,26 @@ import { Link } from 'react-router-dom';
 import './Navigation.css';
 
 function Navigation(props) {
-    return (
-			<div>
-				Hello from Navigation
-				<div className='box'>
-					<div className='box1'></div>
-					<div className='box2'></div>
-					<div className='box3'></div>
-					<div className='box4'></div>
-					<div className='box5'></div>
-				</div>
+	return (
+		<div>
+			Hello from Navigation
+			<div className='box'>
+				<Link to='/'>
+					<div className='box1'>Home</div>
+				</Link>
+				<Link to='/food'>
+					<div className='box2'>Food</div>
+				</Link>
+				<Link to='/order'>
+					<div className='box3'>Order</div>
+				</Link>
+				<Link to='/about'>
+					<div className='box4'>About</div>
+				</Link>
+				<div className='box5'></div>
 			</div>
-		);
+		</div>
+	);
 }
 
 export default Navigation;
